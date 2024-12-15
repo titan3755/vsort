3dd72f0e70e973c6499f033d62e88eb8add0d568
Titan <68808479+titan3755@users.noreply.github.com>
2024-12-15 21:37:11 +0600

    Feature additions
    --> Added hardware accelerated
          graphics functionality
    --> Added Selection and Insertion sort
    --> Added framerate features
    --> Added elapsed time features
    --> Added .ttf font file support for
          future updates
    --> Bug fixes
    --> Appearance improvements
    
    ToDo:
    --> Fix framerate window title bug
    --> Fix insertion sort final array render
          bug (a portion of the rectangles
          remains green while the rest
          becomes blue)
    --> Add more sorting algorithms
    --> Optimize existing code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
-#include <sdl.h>
-#undef main  // Undefine main for SDL2 to work with main function
-#include <SDL_ttf.h>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdio>
+#include <cstdlib>
+#include <chrono>
 #include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <sdl.h>
+#undef main  // Undefine main for SDL2 to work with main function, because SDL2 defines main as SDL_main
+#include <SDL_ttf.h>
 
 constexpr auto WINDOW_WIDTH = 800;
 constexpr auto WINDOW_HEIGHT = 600;
-constexpr auto ARRAY_SIZE = 400;
+constexpr auto ARRAY_SIZE = 100;
+constexpr auto SORTING_ALGORITHM = 3;  // 1 for bubble sort, 2 for selection sort, 3 for insertion sort;
+
 #define BAR_WIDTH (WINDOW_WIDTH / ARRAY_SIZE)
 
 struct Color {
@@ -32,14 +38,41 @@ Color windowClr = {
     0, 0, 0
 };
 
-void draw_bars(SDL_Surface* surface, int* array) {
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        SDL_Rect rect = { i * BAR_WIDTH, WINDOW_HEIGHT - array[i], BAR_WIDTH, array[i] };
-        SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, barClr.r, barClr.g, barClr.b));  // Blue color
+std::mutex mtx;
+
+static void setWindowTitleAccordingToFramerateAndTimeElapsed(SDL_Window* window) {
+    static int frameCount = 0;
+    static int timerFPS = 0;
+    static Uint32 lastFrameTime = 0;
+    static char title[100];
+    frameCount++;
+    Uint32 currentTime = SDL_GetTicks();
+    if (currentTime - timerFPS > 1000) {
+        timerFPS = currentTime;
+		if (SORTING_ALGORITHM == 1) {
+            sprintf_s(title, "Bubble Sort Visualizer - FPS: %d, Frame Time: %d ms", frameCount, (currentTime - lastFrameTime) / frameCount);
+		}
+		else if (SORTING_ALGORITHM == 2) {
+            sprintf_s(title, "Selection Sort Visualizer - FPS: %d, Frame Time: %d ms", frameCount, (currentTime - lastFrameTime) / frameCount);
+		}
+		else if (SORTING_ALGORITHM == 3) {
+			sprintf_s(title, "Insertion Sort Visualizer - FPS: %d, Frame Time: %d ms", frameCount, (currentTime - lastFrameTime) / frameCount);
+		}
+        std::lock_guard<std::mutex> lock(mtx);
+        SDL_SetWindowTitle(window, title);
+        lastFrameTime = currentTime;
+        frameCount = 0;
     }
 }
 
-void bubble_sort_visualized(SDL_Surface* surface, SDL_Window* window, int* array) {
+void draw_bars(SDL_Renderer *renderer, int* array) {
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        SDL_Rect rect = { i * BAR_WIDTH, WINDOW_HEIGHT - array[i], BAR_WIDTH, array[i] };
+		SDL_RenderFillRect(renderer, &rect);
+    }
+}
+
+void bubble_sort_visualized(SDL_Renderer* renderer, SDL_Window* window, int* array) {
     int swapped;
     int temp;
 	SDL_Event event;
@@ -50,7 +83,9 @@ void bubble_sort_visualized(SDL_Surface* surface, SDL_Window* window, int* array
         for (int j = 0; j < ARRAY_SIZE - i - 1; j++) {
 			while (SDL_PollEvent(&event)) {
 				if (event.type == SDL_QUIT) {
+                    SDL_DestroyRenderer(renderer);
 					SDL_DestroyWindow(window);
+					TTF_Quit();
 					SDL_Quit();
 					exit(0);
 				}
@@ -60,11 +95,12 @@ void bubble_sort_visualized(SDL_Surface* surface, SDL_Window* window, int* array
             SDL_Rect rect2 = { (j + 1) * BAR_WIDTH, WINDOW_HEIGHT - array[j + 1], BAR_WIDTH, array[j + 1] };
 
             // Color bars being compared in red
-            SDL_FillRect(surface, &rect1, SDL_MapRGB(surface->format, compClr.r, compClr.g, compClr.b)); // Red color for compared bars
-            SDL_FillRect(surface, &rect2, SDL_MapRGB(surface->format, compClr.r, compClr.g, compClr.b));
+			SDL_SetRenderDrawColor(renderer, compClr.r, compClr.g, compClr.b, 255);
+			SDL_RenderFillRect(renderer, &rect1);
+			SDL_RenderFillRect(renderer, &rect2);
 
             // Update window surface to show changes
-            SDL_UpdateWindowSurface(window);  // Correctly passing the window here
+			SDL_RenderPresent(renderer);
 
             // Compare and swap if needed
             if (array[j] > array[j + 1]) {
@@ -76,11 +112,13 @@ void bubble_sort_visualized(SDL_Surface* surface, SDL_Window* window, int* array
             }
 
             // Redraw the entire array (clear previous coloring)
-            SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, windowClr.r, windowClr.g, windowClr.b)); // Black background
-            draw_bars(surface, array);
+			SDL_SetRenderDrawColor(renderer, windowClr.r, windowClr.g, windowClr.b, 255);
+			SDL_RenderClear(renderer);
+			SDL_SetRenderDrawColor(renderer, barClr.r, barClr.g, barClr.b, 255);
+            draw_bars(renderer, array);
 
             // Update window surface
-            SDL_UpdateWindowSurface(window);  // Correctly passing the window here
+			SDL_RenderPresent(renderer);
         }
         // If no two elements were swapped in the inner loop, the array is sorted
         if (swapped == 0)
@@ -88,16 +126,141 @@ void bubble_sort_visualized(SDL_Surface* surface, SDL_Window* window, int* array
     }
 }
 
+void selection_sort_visualized(SDL_Renderer* renderer, SDL_Window* window, int* array) {
+	int minIndex;
+	int temp;
+	SDL_Event event;
+	// Selection sort algorithm
+	for (int i = 0; i < ARRAY_SIZE - 1; i++) {
+		minIndex = i;
+		for (int j = i + 1; j < ARRAY_SIZE; j++) {
+			while (SDL_PollEvent(&event)) {
+				if (event.type == SDL_QUIT) {
+					SDL_DestroyRenderer(renderer);
+					SDL_DestroyWindow(window);
+					TTF_Quit();
+					SDL_Quit();
+					exit(0);
+				}
+			}
+			// Highlight the two bars being compared
+			SDL_Rect rect1 = { i * BAR_WIDTH, WINDOW_HEIGHT - array[i], BAR_WIDTH, array[i] };
+			SDL_Rect rect2 = { j * BAR_WIDTH, WINDOW_HEIGHT - array[j], BAR_WIDTH, array[j] };
+
+			// Color bars being compared in red
+			SDL_SetRenderDrawColor(renderer, compClr.r, compClr.g, compClr.b, 255);
+			SDL_RenderFillRect(renderer, &rect1);
+			SDL_RenderFillRect(renderer, &rect2);
+
+			// Update window surface to show changes
+			SDL_RenderPresent(renderer);
+
+			// Compare and swap if needed
+			if (array[j] < array[minIndex]) {
+				minIndex = j;
+			}
+
+			// Redraw the entire array (clear previous coloring)
+			SDL_SetRenderDrawColor(renderer, windowClr.r, windowClr.g, windowClr.b, 255);
+			SDL_RenderClear(renderer);
+			SDL_SetRenderDrawColor(renderer, barClr.r, barClr.g, barClr.b, 255);
+			draw_bars(renderer, array);
+
+			// Update window surface
+			SDL_RenderPresent(renderer);
+		}
+		// Swap the elements
+		temp = array[i];
+		array[i] = array[minIndex];
+		array[minIndex] = temp;
+	}
+}
+
+void insertion_sort_visualized(SDL_Renderer* renderer, SDL_Window* window, int* array) {
+	int key;
+	int j;
+	SDL_Event event;
+	// Insertion sort algorithm
+	for (int i = 1; i < ARRAY_SIZE; i++) {
+		key = array[i];
+		j = i - 1;
+		while (j >= 0 && array[j] > key) {
+			while (SDL_PollEvent(&event)) {
+				if (event.type == SDL_QUIT) {
+					SDL_DestroyRenderer(renderer);
+					SDL_DestroyWindow(window);
+					TTF_Quit();
+					SDL_Quit();
+					exit(0);
+				}
+			}
+			// Highlight the two bars being compared
+			SDL_Rect rect1 = { j * BAR_WIDTH, WINDOW_HEIGHT - array[j], BAR_WIDTH, array[j] };
+			SDL_Rect rect2 = { (j + 1) * BAR_WIDTH, WINDOW_HEIGHT - array[j + 1], BAR_WIDTH, array[j + 1] };
+
+			// Color bars being compared in red
+			SDL_SetRenderDrawColor(renderer, compClr.r, compClr.g, compClr.b, 255);
+			SDL_RenderFillRect(renderer, &rect1);
+			SDL_RenderFillRect(renderer, &rect2);
+
+			// Update window surface to show changes
+			SDL_RenderPresent(renderer);
+
+			// Compare and swap if needed
+			array[j + 1] = array[j];
+			j = j - 1;
+
+			// Redraw the entire array (clear previous coloring)
+			SDL_SetRenderDrawColor(renderer, windowClr.r, windowClr.g, windowClr.b, 255);
+			SDL_RenderClear(renderer);
+			SDL_SetRenderDrawColor(renderer, barClr.r, barClr.g, barClr.b, 255);
+			draw_bars(renderer, array);
+
+			// Update window surface
+			SDL_RenderPresent(renderer);
+		}
+		array[j + 1] = key;
+	}
+}
+
 int main() {
+	// get time at start of program
+	auto start = std::chrono::high_resolution_clock::now();
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return -1;
-    }
+	}
+	else {
+		printf("SDL initialized successfully!\n");
+	}
+
+	// Initialize SDL_ttf
+	if (TTF_Init() < 0) {
+		printf("SDL_ttf could not initialize! TTF_Error: %s\n", TTF_GetError());
+		SDL_Quit();
+		return -1;
+	}
+	else {
+		printf("SDL_ttf initialized successfully!\n");
+	}
 
     // Create a window
+	char window_title[] = "Sorting Visualizer";
+	if (SORTING_ALGORITHM == 1) {
+		char window_title[] = "Bubble Sort Visualizer";
+		printf("Bubble Sort Visualizer\n");
+	}
+	else if (SORTING_ALGORITHM == 2) {
+		char window_title[] = "Selection Sort Visualizer";
+		printf("Selection Sort Visualizer\n");
+	}
+	else if (SORTING_ALGORITHM == 3) {
+		char window_title[] = "Insertion Sort Visualizer";
+		printf("Insertion Sort Visualizer\n");
+	}
     SDL_Window* window = SDL_CreateWindow(
-        "Bubble Sort Visualizer",  // Window title
+        window_title,              // Window title
         SDL_WINDOWPOS_UNDEFINED,   // Window position (X)
         SDL_WINDOWPOS_UNDEFINED,   // Window position (Y)
         WINDOW_WIDTH,              // Window width
@@ -107,21 +270,43 @@ int main() {
 
     if (window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		TTF_Quit();
         SDL_Quit();
         return -1;
     }
+	else {
+		printf("Window created successfully!\n");
+	}
 
-    // Get the window surface
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-    if (surface == NULL) {
-        printf("Unable to get window surface! SDL_Error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+	// use hardware acceleration
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == NULL || !renderer) {
+		printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+		SDL_DestroyWindow(window);
+		TTF_Quit();
+		SDL_Quit();
+		return -1;
+	}
+	else {
+		printf("Renderer created successfully!\n");
+	}
+
+	// Set the window title according to the frame rate and time elapsed
+	std::atomic <bool> quit_thread(false);
+    std::thread t([&quit_thread](SDL_Window* window) {
+        while (!quit_thread) {
+            setWindowTitleAccordingToFramerateAndTimeElapsed(window);
+            std::this_thread::sleep_for(std::chrono::microseconds(1)); // Add a small delay to avoid high CPU usage
+        }
+		printf("Thread terminated!\n");
+    }, window);
+
+    // screen flush
+	SDL_SetRenderDrawColor(renderer, windowClr.r, windowClr.g, windowClr.b, 255);
+	SDL_RenderClear(renderer);
 
     // Initialize random seed
-    srand(time(NULL));
+    srand(static_cast<unsigned int>(time(NULL)));
 
     // Generate an array of random heights
     int array[ARRAY_SIZE];
@@ -130,27 +315,65 @@ int main() {
     }
 
     // Draw initial bars
-    draw_bars(surface, array);
-    SDL_UpdateWindowSurface(window);
+	SDL_SetRenderDrawColor(renderer, barClr.r, barClr.g, barClr.b, 255);
+    draw_bars(renderer, array);
+	SDL_RenderPresent(renderer);
     SDL_Delay(1000);  // Delay to let the user see the initial state
 
     // Perform the bubble sort with visualization
-    bubble_sort_visualized(surface, window, array);
-	SDL_UpdateWindowSurface(window);
-
+	if (SORTING_ALGORITHM == 1) {
+		bubble_sort_visualized(renderer, window, array);
+	}
+	else if (SORTING_ALGORITHM == 2) {
+		selection_sort_visualized(renderer, window, array);
+	}
+	else if (SORTING_ALGORITHM == 3) {
+		insertion_sort_visualized(renderer, window, array);
+	}
+	else {
+		printf("Invalid sorting algorithm selected!\n");
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		TTF_Quit();
+		SDL_Quit();
+		return -1;
+	}
+	SDL_RenderPresent(renderer);
 	// draw the final bars with a different color
+	SDL_SetRenderDrawColor(renderer, barFinalClr.r, barFinalClr.g, barFinalClr.b, 255);
 	for (int i = 0; i < ARRAY_SIZE; i++) {
 		SDL_Rect rect = { i * BAR_WIDTH, WINDOW_HEIGHT - array[i], BAR_WIDTH, array[i] };
-		SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, barFinalClr.r, barFinalClr.g, barFinalClr.b));  // Green color
-		SDL_UpdateWindowSurface(window);
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
         SDL_Delay(BAR_WIDTH);
 	}
+	// terminate framerate thread
+	quit_thread = true;
+	// get time at end of program
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = end - start;
+	char window_title_final[100];
+	if (SORTING_ALGORITHM == 1) {
+		sprintf_s(window_title_final, "Bubble Sort Visualizer - Time Elapsed: %f seconds", elapsed.count());
+	}
+	else if (SORTING_ALGORITHM == 2) {
+		sprintf_s(window_title_final, "Selection Sort Visualizer - Time Elapsed: %f seconds", elapsed.count());
+	}
+	else if (SORTING_ALGORITHM == 3) {
+		sprintf_s(window_title_final, "Insertion Sort Visualizer - Time Elapsed: %f seconds", elapsed.count());
+	}
+	else {
+		sprintf_s(window_title_final, "Sorting Visualizer - Time Elapsed: %f seconds", elapsed.count());
+	}
+	SDL_SetWindowTitle(window, "window_title_final");
     // Wait before quitting
     SDL_Event e;
     while (true) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
+				SDL_DestroyRenderer(renderer);
                 SDL_DestroyWindow(window);
+				TTF_Quit();
                 SDL_Quit();
                 exit(0);
             }
@@ -158,8 +381,9 @@ int main() {
     }
 
     // Clean up and close the program
+	SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit();
     SDL_Quit();
-
     return 0;
 }
